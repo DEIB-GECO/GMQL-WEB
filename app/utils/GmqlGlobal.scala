3d0ce9ffcc21678eeea49d25ff12ca0aedd06a9e
acanakoglu <acanak@gmail.com>
2017-06-08 18:40:10 +0200

    select repository type from repository.xml

--- app/utils/GmqlGlobal.scala
+++ app/utils/GmqlGlobal.scala
@@ -1,17 +1,14 @@
 package utils
 
-import java.io.File
-
-import it.polimi.genomics.repository.FSRepository.DFSRepository
-import it.polimi.genomics.repository.Utilities
+import it.polimi.genomics.repository.{GMQLRepository, Utilities}
 import play.api.Play
 import play.api.Play.current
 
 
 object GmqlGlobal {
   Utilities.confFolder = Play.application.getFile("conf/gmql_conf/").getAbsolutePath
-  val repository = new DFSRepository()
   val ut: Utilities = Utilities()
+  val repository: GMQLRepository = ut.getRepository()
   //
 //  val managerUtilities = it.polimi.genomics.manager.Utilities()
 }

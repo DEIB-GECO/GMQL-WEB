8a86d32ec2b3e1e92696b4c2cf685b1d7aa6f7cc
acanakoglu <acanak@gmail.com>
2017-12-25 10:08:33 +0100

    changed countsamp  as builtinconstant, fixes #47

--- public/ace/src/mode-gmql.js
+++ public/ace/src/mode-gmql.js
@@ -1,128 +1,129 @@
-define("ace/mode/gmql_highlight_rules",["require","exports","module","ace/lib/oop","ace/mode/text_highlight_rules"], function(require, exports, module) {
-"use strict";
+define("ace/mode/gmql_highlight_rules",["require","exports","module","ace/lib/oop","ace/mode/text_highlight_rules"], function (require, exports, module) {
+    "use strict";
 
-var oop = require("../lib/oop");
-var TextHighlightRules = require("./text_highlight_rules").TextHighlightRules;
+    var oop = require("../lib/oop");
+    var TextHighlightRules = require("./text_highlight_rules").TextHighlightRules;
 
-var GmqlHighlightRules = function() {
+    var GmqlHighlightRules = function () {
 
-    var keywords = (
-        "select|project|extend|group|merge|order" //relational unary operations
-			+ "|sort" // ?do we have? //relational unary operations
-			+ "|union|difference" //relational binary operations
-			+ "|cover|flat|summit|histogram|map|join" //domain specific operations
-			+ "|materialize" //utility operations
-    );
+        var keywords = (
+            "select|project|extend|group|merge|order" //relational unary operations
+            + "|sort" // ?do we have? //relational unary operations
+            + "|union|difference" //relational binary operations
+            + "|cover|flat|summit|histogram|map|join" //domain specific operations
+            + "|materialize" //utility operations
+        );
 
-    var builtinConstants = (
-        "true|false|distance|mindist|mindistance|dle|dge|md|any|all|start|stop|chr|strand|left|right|up|down|downstream|upstream"
-			+ "|and|or|not|as|in|allbut"
-			+ "|count|bag|sum|avg|min|max|median|std"
-			+ "|cat|contig"
-			+ "|dist|dg|dg|bagd|dl|sqrt"
-			+ "|left_distinct|right_distinct|both"
-    );
-
-    var builtinFunctions = (
-        "region|semijoin" // SELECT
-			+ "|metadata|region_update|metadata_update" // PROJECT
-			+ "|meta_aggregate|region_group|region_aggregate" //GROUP
-			+ "|groupby" // MERGE
-			+ "|desc|meta_top|meta_topg|region_order|region_top|region_topg|" // ORDER
-			+ "|groupby" // MERGE
-			+ "|joinby" //DIFFERENCE
-			+ "|aggregate" //COVER(flat/summit/histogram
-			+ "|output" //JOIN
-            + "|into" //MATERIALIZE
+        var builtinConstants = (
+            "true|false|distance|mindist|mindistance|dle|dge|md|any|all|start|stop|chr|strand|left|right|up|down|downstream|upstream"
+            + "|and|or|not|as|in|allbut"
+            + "|count|bag|sum|avg|min|max|median|std"
             + "|countsamp"
-    );
 
-    var dataTypes = (
-        "int|numeric|decimal|date|varchar|char|bigint|float|double|bit|binary|text|set|timestamp|" +
-        "money|real|number|integer"
-    );
+            + "|cat|contig"
+            + "|dist|dg|dg|bagd|dl|sqrt"
+            + "|left_distinct|right_distinct|both"
+        );
 
-    var keywordMapper = this.createKeywordMapper({
-        "support.function": builtinFunctions,
-        "keyword": keywords,
-        "constant.language": builtinConstants,
-        "storage.type": dataTypes
-    }, "identifier", true);
+        var builtinFunctions = (
+            "region|semijoin" // SELECT
+            + "|metadata|region_update|metadata_update" // PROJECT
+            + "|meta_aggregate|region_group|region_aggregate" //GROUP
+            + "|groupby" // MERGE
+            + "|desc|meta_top|meta_topg|region_order|region_top|region_topg|" // ORDER
+            + "|groupby" // MERGE
+            + "|joinby" //DIFFERENCE
+            + "|aggregate" //COVER(flat/summit/histogram
+            + "|output" //JOIN
+            + "|into" //MATERIALIZE
+        );
 
-    var escapedRe = "\\\\(?:x[0-9a-fA-F]{2}|" + // hex
-        "u[0-9a-fA-F]{4}|" + // unicode
-        "[0-2][0-7]{0,2}|" + // oct
-        "3[0-6][0-7]?|" + // oct
-        "37[0-7]?|" + // oct
-        "[4-7][0-7]?|" + //oct
-        ".)";
+        var dataTypes = (
+            "int|numeric|decimal|date|varchar|char|bigint|float|double|bit|binary|text|set|timestamp|" +
+            "money|real|number|integer"
+        );
 
-    this.$rules = {
-        "start" : [ {
-            token : "comment",
-            regex : "#.*$"
-        }, {
-            token : "string",
-            regex : "'(?=.)",
-            next  : "qstring"
-        }, {
-            token : "string",
-            regex : '"(?=.)',
-            next  : "qqstring"
-        }, {
-            token : "constant.numeric", // float
-            regex : "[+-]?\\d+(?:(?:\\.\\d*)?(?:[eE][+-]?\\d+)?)?\\b"
-        }, {
-            token : keywordMapper,
-            regex : "[a-zA-Z_$][a-zA-Z0-9_$]*\\b"
-        }, {
-            token : "keyword.operator",
-            regex : "\\+|\\-|\\/|\\/\\/|%|<@>|@>|<@|&|\\^|~|<|>|<=|=>|==|!=|<>|=|,|;|:"
-        }, {
-            token : "paren.lparen",
-            regex : "[\\(]"
-        }, {
-            token : "paren.rparen",
-            regex : "[\\)]"
-        }, {
-            token : "text",
-            regex : "\\s+"
-        } ],
-        "qqstring" : [ {
-            token : "constant.language.escape",
-            regex : escapedRe
-        }, {
-            token : "string",
-            regex : "\\\\$",
-            next  : "qqstring"
-        }, {
-            token : "string",
-            regex : '"|$',
-            next  : "no_regex"
-        }, {
-            defaultToken: "string"
-        }],
-        "qstring" : [ {
-            token : "constant.language.escape",
-            regex : escapedRe
-        }, {
-            token : "string",
-            regex : "\\\\$",
-            next  : "qstring"
-        }, {
-            token : "string",
-            regex : "'|$",
-            next  : "no_regex"
-        }, {
-            defaultToken: "string"
-        }]
+        var keywordMapper = this.createKeywordMapper({
+            "support.function": builtinFunctions,
+            "keyword": keywords,
+            "constant.language": builtinConstants,
+            "storage.type": dataTypes
+        }, "identifier", true);
+
+        var escapedRe = "\\\\(?:x[0-9a-fA-F]{2}|" + // hex
+            "u[0-9a-fA-F]{4}|" + // unicode
+            "[0-2][0-7]{0,2}|" + // oct
+            "3[0-6][0-7]?|" + // oct
+            "37[0-7]?|" + // oct
+            "[4-7][0-7]?|" + //oct
+            ".)";
+
+        this.$rules = {
+            "start": [{
+                token: "comment",
+                regex: "#.*$"
+            }, {
+                token: "string",
+                regex: "'(?=.)",
+                next: "qstring"
+            }, {
+                token: "string",
+                regex: '"(?=.)',
+                next: "qqstring"
+            }, {
+                token: "constant.numeric", // float
+                regex: "[+-]?\\d+(?:(?:\\.\\d*)?(?:[eE][+-]?\\d+)?)?\\b"
+            }, {
+                token: keywordMapper,
+                regex: "[a-zA-Z_$][a-zA-Z0-9_$]*\\b"
+            }, {
+                token: "keyword.operator",
+                regex: "\\+|\\-|\\/|\\/\\/|%|<@>|@>|<@|&|\\^|~|<|>|<=|=>|==|!=|<>|=|,|;|:"
+            }, {
+                token: "paren.lparen",
+                regex: "[\\(]"
+            }, {
+                token: "paren.rparen",
+                regex: "[\\)]"
+            }, {
+                token: "text",
+                regex: "\\s+"
+            }],
+            "qqstring": [{
+                token: "constant.language.escape",
+                regex: escapedRe
+            }, {
+                token: "string",
+                regex: "\\\\$",
+                next: "qqstring"
+            }, {
+                token: "string",
+                regex: '"|$',
+                next: "no_regex"
+            }, {
+                defaultToken: "string"
+            }],
+            "qstring": [{
+                token: "constant.language.escape",
+                regex: escapedRe
+            }, {
+                token: "string",
+                regex: "\\\\$",
+                next: "qstring"
+            }, {
+                token: "string",
+                regex: "'|$",
+                next: "no_regex"
+            }, {
+                defaultToken: "string"
+            }]
+        };
+        this.normalizeRules();
     };
-    this.normalizeRules();
-};
 
-oop.inherits(GmqlHighlightRules, TextHighlightRules);
+    oop.inherits(GmqlHighlightRules, TextHighlightRules);
 
-exports.GmqlHighlightRules = GmqlHighlightRules;
+    exports.GmqlHighlightRules = GmqlHighlightRules;
 });
 
 define("ace/mode/folding/gmql",["require","exports","module","ace/lib/oop","ace/range","ace/mode/folding/fold_mode"], function(require, exports, module) {
